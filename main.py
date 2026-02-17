07a1327feb032838dca7c39c6aeb3b9307bb778b
VincentELK <vinidev@outlook.fr>
2026-02-17 11:09:03 +0100

    Mise en place du systeme d'event et de loop

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def main():
     pygame.init()
     print(f"Starting Climber with pygame version: {pygame.version.ver}")
     print(SCREEN_HEIGHT, SCREEN_WIDTH)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            screen.fill((50,45,34))
+            pygame.display.flip()
+
 
 
 if __name__ == "__main__":
