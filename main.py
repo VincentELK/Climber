b1e57468cd957d7ae536385ee4b3c786fc9e4352
VincentELK <vinidev@outlook.fr>
2026-02-20 09:22:17 +0100

    Ajout des actions de directionsm ajout des collisions au bords de l'écran

--- main.py
+++ main.py
@@ -6,17 +6,54 @@ def main():
     print(f"Starting Climber with pygame version: {pygame.version.ver}")
     print(SCREEN_HEIGHT, SCREEN_WIDTH)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Climber")
     running = True
     clock = pygame.time.Clock()
+    fps = clock.get_fps()
     dt = 0
+    player_color = "blue"
+    
+    player_rect = pygame.Rect(100,100,50,50)
+    speed = 5
+    
     while running:
+        
+        dt = clock.tick(60)/1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_RIGHT]:
+            player_rect.x += speed 
+        if key[pygame.K_LEFT]:
+            player_rect.x -= speed 
+        if key[pygame.K_UP]:
+            player_rect.y -= speed 
+        if key[pygame.K_DOWN]:
+            player_rect.y += speed
+        if key[pygame.K_SPACE]:
+            player_color = "red"
+
+
+        if player_rect.x < 0:
+            player_rect.x = 0
+        if player_rect.x > SCREEN_WIDTH - player_rect.width:
+            player_rect.x = SCREEN_WIDTH - player_rect.width
+        
+        if player_rect.y < 0:
+            player_rect.y = 0
+        if player_rect.y > SCREEN_HEIGHT - player_rect.height:
+            player_rect.y = SCREEN_HEIGHT - player_rect.height
+
+        # draw
         screen.fill((50,45,34))
+        pygame.draw.rect(screen, player_color, player_rect)
         pygame.display.flip()
         
-        dt = clock.tick(60)/1000
+        
 
 
 
