74a60ea7ed800ec3de22fd0e054211b8063a8f93
VincentELK <vinidev@outlook.fr>
2026-02-25 16:57:27 +0100

    Remise a zero du code pour repartir sans poo

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from player import Player
-from ladder import Ladder
-from ladder_parts import Part
+
 def main():
     pygame.init()
     print(f"Starting Climber with pygame version: {pygame.version.ver}")
@@ -13,19 +12,7 @@ def main():
     fps = clock.get_fps()
     dt = 0
     
-    
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    parts = pygame.sprite.Group()
-    Ladder.containers = (updatable, drawable)
-    Part.containers = (parts, updatable, drawable )
-    Player.containers = (updatable, drawable)
-    
-    ladder_x = SCREEN_WIDTH / 2
-    
-    ladder = Ladder(ladder_x, 0)
-    
-    player = Player(ladder_x, ladder.parts[-1].position.y)
+
     while running:
         
         dt = clock.tick(60)/1000
@@ -36,9 +23,7 @@ def main():
         
         # draw
         screen.fill((50,45,34))
-        updatable.update(dt)
-        for object in drawable:
-            object.draw(screen)
+     
         
 
         pygame.display.flip()
